b544916e6bc3cfed22dc41ba44f6c6e5e93cdfd6
ocboogie <boogie@mikulec.com>
2020-03-05 02:34:07 -0800

    Remove routes directory

--- server/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handlerGetPrompt } from "./prompt";
-
-const router = Router();
-
-router.get("/prompt", handlerGetPrompt);
-
-export default router;
--- server/src/routes/prompt.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export function handlerGetPrompt(req: Request, res: Response) {
-  return res.json("hello world").status(200);
-}
