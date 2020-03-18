77d59a2c054f7dbd57aaef84aee4992bee264009
ocboogie <boogie@mikulec.com>
2020-03-18 04:15:51 -0700

    Add mailgun configs to the config and allow test mode

--- server/src/config.ts
+++ server/src/config.ts
@@ -9,7 +9,10 @@ const {
   LOG_LEVEL,
   SESSION_LIFETIME,
   REMEMBER_ME_SESSION_LIFETIME,
-  PORT
+  PORT,
+  MAILGUN_API_KEY,
+  MAILGUN_DOAMAIN_NAME,
+  MAILGUN_TEST_MODE
 } = process.env;
 
 export default {
@@ -20,5 +23,8 @@ export default {
   hasingTimeCost: HASING_TIME_COST ? parseInt(HASING_TIME_COST) : 3,
   sessionLifetime: ms(SESSION_LIFETIME || "1d"),
   rememberMeSessionLifetime: ms(REMEMBER_ME_SESSION_LIFETIME || "7d"),
-  logLevel: LOG_LEVEL || "silly"
+  logLevel: LOG_LEVEL || "silly",
+  mailgunApiKey: MAILGUN_API_KEY,
+  mailgunDoamainName: MAILGUN_DOAMAIN_NAME,
+  mailgunTestMode: MAILGUN_TEST_MODE?.toLowerCase() == "true"
 };
--- server/src/loaders/mailgun.ts
+++ server/src/loaders/mailgun.ts
@@ -1,10 +1,23 @@
 import mailgun from "mailgun-js";
+import config from "../config";
+import logger from "./logger";
 
-if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOAMAIN_NAME) {
-  throw "You must supply the MAILGUN_API_KEY and MAILGUN_DOAMAIN_NAME environment variables";
+let apiKey = config.mailgunApiKey;
+let domain = config.mailgunDoamainName;
+let testMode = false;
+
+if (!apiKey || !domain || config.mailgunTestMode) {
+  apiKey = apiKey || "";
+  domain = domain || "";
+  testMode = true;
+  if (!config.development) {
+    throw "You must supply the MAILGUN_API_KEY and MAILGUN_DOAMAIN_NAME environment variables in production";
+  }
+  logger.warn("Runing mailgun in test mode");
 }
 
 export default mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOAMAIN_NAME
+  apiKey,
+  domain,
+  testMode
 });
